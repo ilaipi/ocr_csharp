929491e954837f0bad0a1f53751a404064ea47cd
Billy Yang <mz.yyam@gmail.com>
2020-06-20 21:16:43 +0800

    1234L.jpg 识别成功

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Emgu.CV.OCR;
 using Emgu.CV.Structure;
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Reflection;
 
@@ -12,12 +13,74 @@ namespace ocr
     {
         static void Main(string[] args)
         {
-            string imagePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "..", "assets", "123.png");
-            Bitmap image = (Bitmap)Image.FromFile(imagePath);
-            //Console.WriteLine(image.Width);
+            string imagePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..", "..", "assets", "1234L.jpg");
+            Image file = Image.FromFile(imagePath);
+            Bitmap image = new Bitmap(file);
 
             Util.Gray(image);
-            image.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new.png"));
+            byte threshold = Util.Otsu(image);
+            //Console.WriteLine("threshold is: " + threshold);
+            Util.Thresholding(image, threshold);
+
+            // 这一步调用 ToImage 之后 图片会自动逆时针转90度
+            var image1 = image.ToImage<Bgr, byte>();
+            image1 = image1.Rotate(90, new Bgr(0, 0, 0));
+
+            image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new0.png"));
+
+            // 这里new Size(3, 3) 的3  很关键，好像太大 太小 都找不对顶点
+            CvInvoke.GaussianBlur(image1, image1, new Size(3, 3), 3);
+
+            image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new1.png"));
+           
+            Point left = new Point();
+            Point right = new Point();
+            image1 = Util.FindVertices(image1, ref left, ref right);
+            image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new2.png"));
+            Point[] points = new Point[2];
+            points[0] = left;
+            points[1] = right;
+            //image1.DrawPolyline(points, true, new Bgr(Color.Red), 10);
+            image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new3.png"));
+
+            Console.WriteLine("1 left: " + left.X + "  " + left.Y);
+            Console.WriteLine("1 right: " + right.X + "  " + right.Y);
+
+            double angle = Util.getAngleTray(left, right, left, new Point(right.X, left.Y));
+
+            Console.WriteLine("angle is: " + angle);
+
+            // 旋转
+            image1 = image1.Rotate(angle, new Bgr(0, 0, 0));
+            image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new4.png"));
+
+
+            image1 = Util.FindVertices(image1, ref left, ref right);
+
+            int cutWidth = 700;
+            int cutHeight = 800;
+            Bitmap MasterMap = new Bitmap(cutWidth, cutHeight, PixelFormat.Format32bppRgb);
+            Graphics g = Graphics.FromImage(MasterMap);
+
+            Rectangle dest = new Rectangle(0, 0, cutWidth, cutHeight);
+            Rectangle source = new Rectangle(left.X + 300, left.Y + 200, cutWidth, cutHeight);
+            g.DrawImage(image1.AsBitmap(), dest, source, GraphicsUnit.Pixel);
+            MasterMap.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new5.png"));
+
+            // 这里调用后 图片没有旋转  不知道为什么
+            image1 = MasterMap.ToImage<Bgr, byte>();
+
+            #region 开操作 没什么效果    https://www.cnblogs.com/ssyfj/p/9277688.html
+            //var image1 = image.ToImage<Bgr, byte>();
+            //Mat kernel1 = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(10, 10), new Point(0, 0));
+            //image1.MorphologyEx(Emgu.CV.CvEnum.MorphOp.Gradient, kernel1, new Point(0, 0), 1, Emgu.CV.CvEnum.BorderType.Default, new MCvScalar());
+            //image1.Save(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "new.png"));
+            #endregion
+
+            string winName = "test-win";
+            CvInvoke.NamedWindow(winName, Emgu.CV.CvEnum.WindowFlags.Normal);
+            CvInvoke.Imshow(winName, image1);
+            
 
             string ocrPath = "./Properties/tessdata";
             //string language = "chi_sim";
@@ -25,7 +88,7 @@ namespace ocr
 
             Tesseract ocr = new Tesseract(ocrPath, language, OcrEngineMode.Default);
 
-            ocr.SetImage(image.ToImage<Bgr, byte>());
+            ocr.SetImage(image1);
             ocr.Recognize();
 
             Tesseract.Character[] characters = ocr.GetCharacters();
@@ -43,6 +106,9 @@ namespace ocr
                 Console.WriteLine("Error");
                 return;
             }
+
+            CvInvoke.WaitKey(0);
+            CvInvoke.DestroyWindow(winName);
         }
     }
 }
--- Util.cs
+++ Util.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -10,6 +14,8 @@ namespace ocr
 {
     class Util
     {
+
+        #region 灰阶
         public static Bitmap Gray(Bitmap image)
         {
             BitmapData bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
@@ -37,5 +43,175 @@ namespace ocr
             image.UnlockBits(bitmapData);
             return image;
         }
+        #endregion
+
+        #region 固定阈值 二值化
+        public static Bitmap Thresholding(Bitmap image, byte threshold)
+        {
+            BitmapData bitmapData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            unsafe
+            {
+                byte* p = (byte*)bitmapData.Scan0;
+                int offset = bitmapData.Stride - image.Width * 4;
+                byte R, G, B, gray;
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        R = p[2];
+                        G = p[1];
+                        B = p[0];
+                        gray = (byte)((R * 19595 + G * 38469 + B * 7472) >> 16);
+                        if (gray >= threshold)
+                        {
+                            p[0] = p[1] = p[2] = 255;
+                        }
+                        else
+                        {
+                            p[0] = p[1] = p[2] = 0;
+                        }
+                        p += 4;
+                    }
+                    p += offset;
+                }
+            }
+            image.UnlockBits(bitmapData);
+            return image;
+        }
+        #endregion
+
+        #region otsu阈值法 二值化
+        public static byte Otsu(Bitmap image)
+        {
+            byte threshold = 0;
+            int width = image.Width;
+            int height = image.Height;
+            int[] hist = new int[256];
+            int AllPixelNumber = 0, PixelNumberSmall = 0, PixelNumberBig = 0;
+            double MaxValue, AllSum = 0, SumSmall = 0, SumBig, ProbabilitySmall, ProbabilityBig, Probability;
+            BitmapData data = image.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            unsafe
+            {
+                byte* p = (byte*)data.Scan0;
+                int offset = data.Stride - width * 4;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        hist[p[0]]++;
+                        p += 4;
+                    }
+                    p += offset;
+                }
+                image.UnlockBits(data);
+
+                //计算灰度为I的像素出现的概率
+                for (int i = 0; i < 256; i++)
+                {
+                    AllSum += i * hist[i];     //   质量矩
+                    AllPixelNumber += hist[i];  //  质量
+                }
+
+                MaxValue = -1.0;
+                for (int i = 0; i < 256; i++)
+                {
+                    PixelNumberSmall += hist[i];
+                    PixelNumberBig = AllPixelNumber - PixelNumberSmall;
+                    if (PixelNumberBig == 0)
+                    {
+                        break;
+                    }
+                    SumSmall += i * hist[i];
+                    SumBig = AllSum - SumSmall;
+                    ProbabilitySmall = SumSmall / PixelNumberSmall;
+                    ProbabilityBig = SumBig / PixelNumberBig;
+                    Probability = PixelNumberSmall * ProbabilitySmall * ProbabilitySmall + PixelNumberBig * ProbabilityBig * ProbabilityBig;
+                    if (Probability > MaxValue)
+                    {
+                        MaxValue = Probability;
+                        threshold = (byte)i;
+                    }
+                }
+            }
+            return threshold;
+        }
+        #endregion
+
+        public static double getAngleTray(Point mac0, Point mac1, Point maxp0, Point maxp1)
+        {
+            var a = Math.Atan2(mac1.Y - mac0.Y, mac1.X - mac0.X);
+            var b = Math.Atan2(maxp1.Y - maxp0.Y, maxp1.X - maxp0.X);
+            double angle = (double)(180 * (b - a)) / (double)Math.PI;
+            if (angle > 180)
+                angle -= 360;
+            if (angle < -180)
+                angle += 360;
+            return angle;
+        }
+
+        #region FindVertices
+        public static Image<Bgr, byte> FindVertices(Image<Bgr, byte> image, ref Point left, ref Point right)
+        {
+            #region Canny and edge detection
+            UMat cannyEdges = new UMat();
+
+            CvInvoke.Canny(image, cannyEdges, 60, 180);
+            #endregion
+
+            List<RotatedRect> boxList = new List<RotatedRect>(); //旋转的矩形框
+            Point center = new Point(); ;
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.08, true);
+                        var area = CvInvoke.ContourArea(approxContour, false);
+                        //仅考虑面积大于300000的轮廓
+                        if (area > 100000)
+                        {
+                            if (approxContour.Size == 4) //轮廓有4个顶点
+                            {
+
+                                Point[] pts = approxContour.ToArray();
+                                //求四边形中心点？坐标
+                                int x_average = 0;
+                                int y_average = 0;
+                                int x_sum = 0;
+                                int y_sum = 0;
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    x_sum += pts[j].X;
+                                    y_sum += pts[j].Y;
+                                }
+                                x_average = x_sum / 4;
+                                y_average = y_sum / 4;
+                                center = new Point(x_average, y_average);
+                                for (int j = 0; j < 4; j++)
+                                {
+                                    if (pts[j].X < center.X && pts[j].Y < center.Y)
+                                    {
+                                        left = pts[j];//左上角点
+                                    }
+                                    if (pts[j].X > center.X && pts[j].Y < center.Y)
+                                    {
+                                        right = pts[j];//右上角点
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            cannyEdges.Dispose();
+            return image;
+        }
+        #endregion
     }
 }
